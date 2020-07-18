46f5d6b2babdb95547b79448675d4e95cc650abd
wintyo <wintyo1024@gmail.com>
2020-07-18 11:17:34 +0900

    カーソルキーで前後の日付に移動できるようにした

--- src/scripts/components/DayList.tsx
+++ src/scripts/components/DayList.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classNames from 'classnames';
 import { times } from 'lodash-es';
-import { format as formatDate, isSameDay, getDaysInMonth } from 'date-fns';
+import { format as formatDate, isSameDay, getDaysInMonth, addDays } from 'date-fns';
 
 import styles from './css/DayList.scss';
 
 // interfaces
-// import { ITextMap } from '~/interfaces/App';
-import { ITextMap } from '../interfaces/App';
+import { ITextMap } from '~/interfaces/App';
 
 interface IProps {
   /** 表示する月 */
@@ -25,17 +24,36 @@ const DayList = (props: IProps) => {
   const month = props.targetMonth.getMonth();
   const numDays = getDaysInMonth(props.targetMonth);
 
+  const els = useRef<React.RefObject<HTMLDivElement>[]>([]);
+  times(numDays).forEach((i) => {
+    els.current[i] = React.createRef<HTMLDivElement>();
+  });
   return (
     <div className={styles.list}>
-      {times(numDays).map((day) => {
-        const date = new Date(year, month, day + 1);
+      {times(numDays).map((index) => {
+        const date = new Date(year, month, index + 1);
         const title = (props.textMap[formatDate(date, 'yyyyMMdd')] || '').split('\n')[0];
         return (
-          <div key={day} className={styles.list__item}>
+          <div key={index} className={styles.list__item}>
             <div
+              ref={els.current[index]}
+              tabIndex={0}
               className={classNames(styles.day, {
                 [styles._selected]: isSameDay(date, props.selectedDate),
               })}
+              onKeyDown={(event) => {
+                event.preventDefault();
+                if (event.key === 'ArrowUp' && index > 0) {
+                  els.current[index - 1].current?.focus();
+                  props.onChangeSelectedDate(addDays(date, -1));
+                  return;
+                }
+                if (event.key === 'ArrowDown' && index < numDays - 1) {
+                  els.current[index + 1].current?.focus();
+                  props.onChangeSelectedDate(addDays(date, 1));
+                  return;
+                }
+              }}
               onClick={() => { props.onChangeSelectedDate(date); }}
             >
               <div className={styles.day__date}>{formatDate(date, 'yyyy/MM/dd')}</div>
