e168fa6c49e908eca40196660bc76430ddf68424
wintyo <wintyo1024@gmail.com>
2021-05-03 07:44:01 +0900

    firebaseの設定情報の登録
    設定情報は隠していてもビルド時に含まれてしまうので、gitに含めることにした

---
+++ config/firebase.ts
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyACeSFFTi3J7xL1dPeDWbyuSftacQeWuHI",
+  authDomain: "fir-diary-b3104.firebaseapp.com",
+  databaseURL: "https://fir-diary-b3104.firebaseio.com",
+  projectId: "fir-diary-b3104",
+  storageBucket: "fir-diary-b3104.appspot.com",
+  messagingSenderId: "946570334307",
+  appId: "1:946570334307:web:b87b89fcb5efd59a18a856",
+  measurementId: "G-2YDCVZ68L5"
+};
