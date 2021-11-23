3f1aa0ba514602aa27b64ff24ab30a3a7c3bbc3f
wintyo <wintyo1024@gmail.com>
2021-11-23 17:43:27 +0900

    環境を切り替えてビルド、デプロイできるようにした

--- scripts/outFirebaseConfig.ts
+++ scripts/outFirebaseConfig.ts
@@ -1,11 +1,17 @@
 import { promises as fsPromises } from 'fs'
 const firebaseTools = require('firebase-tools');
 
+const env = process.argv[2];
+console.log('env:', env);
+
 (async () => {
+  const firebaseRC = JSON.parse(await fsPromises.readFile('./.firebaserc', 'utf-8'));
+  const projectId = firebaseRC.projects[env];
+  console.log('projectId:', projectId);
+  await firebaseTools.use(projectId, {});
   const apps = await firebaseTools.apps.list('WEB', {});
-  console.log(apps);
   const currentApp = apps[0];
   const config = await firebaseTools.apps.sdkconfig('WEB', currentApp.appId, {});
-  console.log(config);
-  await fsPromises.writeFile('./config/sdk.json', JSON.stringify(config.sdkConfig, null, 2));
+  console.log(config.sdkConfig);
+  await fsPromises.writeFile('./config/sdkConfig.json', JSON.stringify(config.sdkConfig, null, 2));
 })();
--- src/scripts/firebase/index.ts
+++ src/scripts/firebase/index.ts
@@ -4,7 +4,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
-import config from '../../../config/firebase';
+import config from '../../../config/sdkConfig.json';
 
 firebase.initializeApp(config);
 
