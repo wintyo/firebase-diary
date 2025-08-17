34b758dd84de99fb248806a946351e78bb6e5b1f
wintyo <wintyo1024@gmail.com>
2025-08-17 10:26:32 +0900

    スタイルをリセットした

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import type { User } from "firebase/auth";
@@ -11,7 +9,6 @@ import { auth, authProviders, database } from "./firebase";
 function App() {
   const [isAuthorizing, setIsAuthorizing] = useState(true);
   const [user, setUser] = useState<User | null>(null);
-  const [count, setCount] = useState(0);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -43,30 +40,7 @@ function App() {
     );
   }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div>ログイン完了</div>;
 }
 
 export default App;
